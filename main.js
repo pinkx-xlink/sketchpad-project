1f2196966be129f22e5b9ba615421024d820a09d
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-06-27 21:17:36 -0400

    Make grid adjustable

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-let canvas = document.querySelector('.canvas');
-canvas.style.gridTemplateColumns = repeat(16 , 1fr);
-canvas.style.gridTemplateRows = repeat(16 , 1fr);
+function populateCanvas(size) {
+    let canvas = document.querySelector('.canvas');
+canvas.style.gridTemplateColumns = `repeat$(size , 1fr)`;
+canvas.style.gridTemplateRows = `repeat$(size , 1fr)`;
 
-for(let i = 0; i<256; i++) {
-    let square = document.createElement('div');
-    square.style.backgroundColor = 'red';
+for (let i = 0; i < 256; i++) {
+    let square = document.createElement("div");
+    square.style.backgroundColor = "red";
     canvas.insertAdjacentElement('beforeend', square);
+}
 }
