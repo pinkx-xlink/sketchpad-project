bc8fb6314f58cf2d2a851520a13ec166d086e30b
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-06-27 21:28:28 -0400

    Add h1 & function to set canvas size

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 function populateCanvas(size) {
-    let canvas = document.querySelector('.canvas');
-canvas.style.gridTemplateColumns = `repeat$(size , 1fr)`;
-canvas.style.gridTemplateRows = `repeat$(size , 1fr)`;
+    let canvas = document.querySelector(".canvas");
+    canvas.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    canvas.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement("div");
-    square.style.backgroundColor = "red";
-    canvas.insertAdjacentElement('beforeend', square);
+    for (let i = 0; i < 256; i++) {
+        let square = document.createElement("div");
+        square.style.backgroundColor = "red";
+        canvas.insertAdjacentElement('beforeend', square);
+    }
 }
+
+populateCanvas(16);
+
+function changeSize(input) {
+    populateCanvas(input);
 }
